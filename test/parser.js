8a0e58f69302465f91e20b7a580589a03b628164
Jeremy Haak <jeremyrhaak@gmail.com>
2020-12-18 16:33:09 -0500

    Implement Interval symbol in parser

--- test/parser.js
+++ test/parser.js
@@ -1,64 +1,61 @@
 import { describe, it } from 'mocha';
 import assert from 'assert';
 import Parser from '../lib/parser';
+import { Interval } from '../lib/nodes';
 
 const parser = new Parser();
 
 describe('Durations', () => {
   it('seconds', () => {
-    const actual = parser.parse(`35 sec`);
+    const actual = parser.parse(`35 sec 350W`);
     const expected = {
       type: 'Program',
-      body: 35,
+      body: Interval(35, { power: 350 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
-  it(
-    'throw error with seconds as float' /* Current parser implementation causes this to fail
-    () => {
-    assert.throws(() => parser.parse(`3.5 sec`));
-  } 
-  */
-  );
+  it('throw error with seconds as float', () => {
+    assert.throws(() => parser.parse(`3.5 sec @200W`));
+  });
   it('minutes', () => {
-    const actual = parser.parse(`2 Minutes`);
+    const actual = parser.parse(`2 Minutes @200W`);
     const expected = {
       type: 'Program',
-      body: 120,
+      body: Interval(120, { power: 200 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
   it('hours', () => {
-    const actual = parser.parse(`1.5 HRS`);
+    const actual = parser.parse(`1.5 HRS @ 0.90`);
     const expected = {
       type: 'Program',
-      body: 5400,
+      body: Interval(5400, { percentFTP: 0.9 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
 });
 describe('Intensity', () => {
   it('power', () => {
-    const actual = parser.parse(`350W`);
+    const actual = parser.parse(`15s 350W`);
     const expected = {
       type: 'Program',
-      body: { power: 350 },
+      body: Interval(15, { power: 350 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
   it('percentFTP as percentage', () => {
-    const actual = parser.parse(`85%`);
+    const actual = parser.parse(`120s 85%`);
     const expected = {
       type: 'Program',
-      body: { percentFTP: 0.85 },
+      body: Interval(120, { percentFTP: 0.85 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
   it('percentFTP as plain number', () => {
-    const actual = parser.parse(`1.35`);
+    const actual = parser.parse(`30s 1.35`);
     const expected = {
       type: 'Program',
-      body: { percentFTP: 1.35 },
+      body: Interval(30, { percentFTP: 1.35 }),
     };
     assert.deepStrictEqual(actual, expected);
   });
