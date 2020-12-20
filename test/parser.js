5559eb087d00811bdf7f34b23b350183e6c8906e
Jeremy Haak <jeremyrhaak@gmail.com>
2020-12-20 11:37:29 -0500

    Handle Blocks and Set
    Revised the code for handling Blocks and Sets. A Block is both a list of
    intervals and a list of sets. A set is a repeat followed by a block.
    Simplified output to simply be a Block at top level.
    Updated tests.

--- test/parser.js
+++ test/parser.js
@@ -8,10 +8,7 @@ const parser = new Parser();
 describe('Durations', function () {
   it('seconds', function () {
     const actual = parser.parse(`35 sec 350W`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(35, { power: 350 })]),
-    };
+    const expected = [Interval(35, { power: 350 })];
     assert.deepStrictEqual(actual, expected);
   });
   it('throw error with seconds as float', function () {
@@ -19,44 +16,29 @@ describe('Durations', function () {
   });
   it('minutes', function () {
     const actual = parser.parse(`2 Minutes @200W`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(120, { power: 200 })]),
-    };
+    const expected = [Interval(120, { power: 200 })];
     assert.deepStrictEqual(actual, expected);
   });
   it('hours', function () {
     const actual = parser.parse(`1.5 HRS @ 0.90`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(5400, { percentFTP: 0.9 })]),
-    };
+    const expected = [Interval(5400, { percentFTP: 0.9 })];
     assert.deepStrictEqual(actual, expected);
   });
 });
 describe('Intensity', function () {
   it('power', function () {
     const actual = parser.parse(`15s 350W`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(15, { power: 350 })]),
-    };
+    const expected = [Interval(15, { power: 350 })];
     assert.deepStrictEqual(actual, expected);
   });
   it('percentFTP as percentage', function () {
     const actual = parser.parse(`120s 85%`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(120, { percentFTP: 0.85 })]),
-    };
+    const expected = [Interval(120, { percentFTP: 0.85 })];
     assert.deepStrictEqual(actual, expected);
   });
   it('percentFTP as plain number', function () {
     const actual = parser.parse(`30s 1.35`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(30, { percentFTP: 1.35 })]),
-    };
+    const expected = [Interval(30, { percentFTP: 1.35 })];
     assert.deepStrictEqual(actual, expected);
   });
 });
@@ -64,18 +46,12 @@ describe('Intensity', function () {
 describe('Intervals', function () {
   it('interval without annotation', function () {
     const actual = parser.parse(`1min @200W`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(60, { power: 200 })]),
-    };
+    const expected = [Interval(60, { power: 200 })];
     assert.deepStrictEqual(actual, expected);
   });
   it('interval with annotation', function () {
     const actual = parser.parse(`1min @200W "max effort"`);
-    const expected = {
-      type: 'Program',
-      body: Set(1, [Interval(60, { power: 200 }, 'max effort')]),
-    };
+    const expected = [Interval(60, { power: 200 }, 'max effort')];
     assert.deepStrictEqual(actual, expected);
   });
 });
@@ -83,21 +59,37 @@ describe('Intervals', function () {
 describe('Sets', function () {
   it('simple single-line set', function () {
     const actual = parser.parse(`2x 1min @200W`);
-    const expected = {
-      type: 'Program',
-      body: Set(2, [Interval(60, { power: 200 })]),
-    };
+    const expected = [Set(2, [Interval(60, { power: 200 })])];
     assert.deepStrictEqual(actual, expected);
   });
   it('single-line set with 2 intervals', function () {
     const actual = parser.parse(`2x 1min @200W, 2min @170W`);
-    const expected = {
-      type: 'Program',
-      body: Set(2, [Interval(60, { power: 200 }), Interval(120, { power: 170 })]),
-    };
+    const expected = [
+      Set(2, [Interval(60, { power: 200 }), Interval(120, { power: 170 })]),
+    ];
     assert.deepStrictEqual(actual, expected);
   });
   it('should not allow float repeats', function () {
     assert.throws(() => parser.parse(`2.5x 1min @200W`));
+  });
+  it('single indent block', function () {
+    const actual = parser.parse(`2x\n  1min @200W\n  2min @170W\n30s @ 120W`);
+    const expected = [
+      Set(2, [Interval(60, { power: 200 }), Interval(120, { power: 170 })]),
+      Interval(30, { power: 120 }),
+    ];
+    assert.deepStrictEqual(actual, expected);
+  });
+  it('double indent block', function () {
+    const actual = parser.parse(
+      `2x\n  1min @200W\n  3x\n    2min @170W\n    30s @ 120W`
+    );
+    const expected = [
+      Set(2, [
+        Interval(60, { power: 200 }),
+        Set(3, [Interval(120, { power: 170 }), Interval(30, { power: 120 })]),
+      ]),
+    ];
+    assert.deepStrictEqual(actual, expected);
   });
 });
