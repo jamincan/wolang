cac1156b01edb64dbfc2b6d2ae1975087b8ec459
Jeremy Haak <jeremyrhaak@gmail.com>
2020-12-31 10:20:41 -0500

    Implement basic compiler

--- demo/src/routes/demo.svelte
+++ demo/src/routes/demo.svelte
@@ -4,58 +4,96 @@
 
   const parser = new Parser();
 
-  let ftp = 200;
-  let input = '1 min @ 200W';
-  let output = compile(input);
+  let program = '';
+  let error = '';
+  $: output = compile(program);
 
-  // Flattens a block so that there are no nested sets in it
+  // Recursively flatten a block so that there are no nested sets in it
   function flatten(block) {
     return block.flatMap((set) => {
       if (set.type !== 'Set') return [set];
       const elements = flatten(set.sets);
-      return Array(set.repeat).flatMap(() => elements);
+      console.log(elements);
+      return Array(set.repeat)
+        .fill()
+        .flatMap(() => elements);
     });
   }
 
   function compile(program) {
+    // Parse the program and catch any errors
+    try {
+      const ast = parser.parse(program);
+    } catch (e) {
+      error = e;
+      return;
+    }
+
     const intervals = flatten(parser.parse(program));
 
+    // Scan to ensure intensity is all power or all percentFTP
+    const hasPower = intervals.some((interval) => interval.intensity.type === 'Power');
+    const hasPercentFTP = intervals.some(
+      (interval) => interval.intensity.type === 'PercentFTP'
+    );
+    if (hasPower && hasPercentFTP) {
+      error =
+        ".zwo files can't mix relative and absolute values. Specify power only as a percentage or absolute power, but not both.";
+      return;
+    }
+
+    // If the program parses, clear the error
+    error = '';
+
+    // Build the workout xml structure from the list of intervals
     const workout = intervals.map(({ duration, intensity, annotation }) => {
-      console.log(intensity);
-      intensity =
-        intensity.type === 'PercentFTP' ? intensity.value : intensity.power / ftp;
-      let interval = { SteadyState: { '@Duration': duration, '@Power': intensity } };
+      let interval = {
+        SteadyState: { '@Duration': duration, '@Power': intensity.value },
+      };
       if (annotation) {
-        interval['textevent'] = { '@timeoffset': 0, message: annotation };
+        interval['SteadyState']['textevent'] = {
+          '@timeoffset': 0,
+          '@message': annotation,
+        };
       }
       return interval;
     });
-    const doc = create({
+
+    // Build the full workout_file document
+    let doc = {
       workout_file: {
-        author: '',
-        name: '',
-        description: '',
+        author: 'Insert your name here.',
+        name: 'Insert workout name here.',
+        description: program,
         sportType: 'bike',
-        tags: '',
-        workout,
+        workout: {
+          '#': workout,
+        },
       },
-    });
-    return doc.end({ prettyPrint: true });
+    };
+    if (hasPower) doc['workout_file']['ftpOverride'] = 1; // If absolute values are specified, set FTP to 1 so that power values convert properly
+
+    return create(doc).end({ prettyPrint: true });
   }
 </script>
 
 <section class="input">
   <form>
-    <label for="ftp">FTP:</label>
-    <input type="number" {ftp} />
-    <label for="input">Wolang program:</label>
-    <textarea id="input"> {input} </textarea>
+    <textarea
+      id="input"
+      bind:value={program}
+      placeholder="Enter your wolang program here"
+      style="width: 100%; height: 20em" />
   </form>
 </section>
-<section class="output">
-  <pre>
+{#if error}
+  <p>{error}</p>
+{:else}
+  <section class="output">
+    <pre>
   <code>
     {output}
   </code>
 </pre>
-</section>
+  </section>
+{/if}
